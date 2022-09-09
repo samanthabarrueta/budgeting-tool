47aaa68646776d7d8bf4e0e9912cf31eb7f575a0
Samantha <samanthabarrueta@gmail.com>
2022-09-08 22:00:25 -0500

    add input for month header w/save btn
    allows user to append month name to month header on click of save button/ hides save button and input box

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
-$(document).ready(function () {
+document.getElementById("saveCurrentMonthBtn").onclick = function () {saveCurrentMonth()};
 
-});
+function saveCurrentMonth() {
+    document.getElementById("currentMonth").style.display = "none";
+    document.getElementById("saveCurrentMonthBtn").style.display = "none";
+    let monthForHeader = document.getElementById("currentMonth").value;
+    document.getElementById("monthHeader").append(monthForHeader);
+    console.log(monthForHeader);
+};
