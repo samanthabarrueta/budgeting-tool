6b9b2fd4e9c7f68c674978a3ba428370ca191953
Samantha <samanthabarrueta@gmail.com>
2022-09-11 21:57:47 -0500

    toggle input value/input field on click of save/edit buttons

--- script.js
+++ script.js
@@ -1,14 +1,24 @@
-const monthHeader = document.getElementById('monthHeader');
+const monthLabel = document.getElementById('monthLabel');
+const monthInputValue = document.getElementById('monthInputValue');
 const monthInputField = document.getElementById('monthInputField');
 const saveMonthInputBtn = document.getElementById('saveMonthInputBtn');
 const editMonthInputBtn = document.getElementById('editMonthInputBtn');
 
+
 const saveCurrentMonth = () => {
     editMonthInputBtn.style.display = 'block';
     saveMonthInputBtn.style.display = 'none';
+    let monthInput = document.getElementById('monthInputField').value;
+    monthInputValue.style.display = 'inline-block';
+    monthInputValue.append(' ' + monthInput);
+    monthInputField.style.display = 'none';
+    monthInputValue.innerHTML = monthInput;
 };
 
 const editCurrentMonth = () => {
-    saveMonthInputBtn.style.display = 'block';
+    saveMonthInputBtn.style.display = 'inline-block';
     editMonthInputBtn.style.display = 'none';
+    monthInputField.style.display = 'inline-block';
+    monthInputValue.style.display = 'none';
 };
+
